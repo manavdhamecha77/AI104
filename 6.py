bb2a00f7dd44b61ea82a0897df44beff8642c0ef
Mr. Manav Dhamecha <u24ai034@aid.svnit.ac.in>
2025-01-15 21:08:28 +0530

    Delete 6.py

--- 6.py
+++
@@ -1,5 +0,0 @@
-num = input("Enter any number : ")  # Accepting user input as a string
-
-num = num[::-1]  # Reversing the number as string
-
-print("Reverse of the given number is :",int(num))  # Type cast string to integer
