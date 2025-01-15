a6fd1f294cc5efd8bc3bf904162b002b830d07b4
Mr. Manav Dhamecha <u24ai034@aid.svnit.ac.in>
2025-01-15 21:07:53 +0530

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-#Input two numbers from user
-a = int(input("Enter number 1: "))
-b = int(input("Enter number 2: "))
-
-#Addition logic
-sum = a + b
-
-#Display the result 
-print("Addition of given two numbers is :",sum)
